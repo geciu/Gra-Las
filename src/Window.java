97b162a3976efbc930ec4f73206c036f370096d7
geciu <getekf@gmail.com>
2018-10-30 23:18:05 +0100

    Update Main file

---
+++ src/Window.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class Window {
+    Window(String nazwa){
+        super(nazwa); //metoda super wywołuje konstruktor nadklasy
+        setResizable(false);
+        setSize(400,400);
+
+}
