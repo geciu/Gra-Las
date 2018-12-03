5d86280a766526c73ef44883242df60858097780
geciu <getekf@gmail.com>
2018-12-03 23:30:49 +0100

    Update main file

--- src/LasMain.java
+++ src/LasMain.java
@@ -6,14 +6,20 @@ public class LasMain {
     public static void main(String[] args) {
         System.out.println("Witaj w grze Las!");
 
-
-       /* Window w = new Window("Witaj w grze Las!");
+/*
+        Window w = new Window(" ");
+        Button b = new Button("Dalej");
+        w.add(b);
+        b.setSize(300, 100);
         w.setVisible(true);
 */
+
+
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
                 new Window("Witaj w grze Las!");
+                new ActionFrame();
             }
         });
     }
